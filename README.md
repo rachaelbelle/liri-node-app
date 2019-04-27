348e37ce9f0ad9b669b074bd1b90ba00e4d57b7c
YvelStLaurent <rachaelbelle@gmail.com>
2019-04-26 22:07:25 -0400

    most functionality

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# liri-node-app
+# LIRI Bot
+
+### Overview
+
+This is LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface.
+
+### Welcome
+
+LIRI can search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+She send requests using the `axios` package to the Bands in Town, Spotify and OMDB APIs. You'll find these Node packages crucial for your assignment.
+
+To test my app, please install the dependencies found in my package.JSON.  (I have git ignored my Node Modules)
--- liri.js
+++ liri.js
@@ -4,18 +4,23 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var choice = process.argv[2];
 var details = process.argv[3];
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+var moment = require("moment")
+
 
 // console.log(choice);
 if (choice === "spotify-this-song") {
     // console.log("spotify");
     spotifyThis();
 } else if (choice === "concert-this") {
-    console.log("concert")
+    // console.log("concert")
+    concert();
 } else if (choice === "movie-this") {
     movie();
     console.log("movie")
 } else if (choice === "do-what-it-says") {
+    something();
     console.log("something")
 } else {
     console.log("invalid command")
@@ -31,12 +36,30 @@ function spotifyThis() {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+            // console.log(JSON.stringify(data.tracks.items));
+            console.log("Artists Name : " + data.tracks.items[0].album.artists[0].name);
+            console.log("Song Name : " + data.tracks.items[0].album.name);
+            console.log("Preview Link : " + data.tracks.items[0].external_urls.spotify);
+            console.log("Album Name : " + data.tracks.items[0].name);
 
-        console.log(data.tracks.items[0]);
-        console.log("Album Name : " + data.tracks.items[0].album.name)
+
     });
 }
-// var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+function concert() {
+    var bandsInTown = "https://rest.bandsintown.com/artists/" + details + "/events?app_id=codingbootcamp";
+    // console.log(bandsInTown)
+    axios.get(bandsInTown).then(function (response) {
+        var data = response.data
+        //  console.log(response.data)
+        //  console.log("console logged" + response.data.Venue)
+        console.log("this worked: " + data.venue.name)
+    }).catch(function (err) {
+        console.log("err");
+    });
+}
+// I have tried everything I can think of and this still will not work.  I am unsure of
+// how to move past it.
 
 
 function movie() {
@@ -53,6 +76,20 @@ function movie() {
         console.log("Plot of Film : " + data.Plot)
         console.log("Actors in Film : " + data.Actors)
     }).catch(function (err) {
-        console.log(err);
-    })
+        console.log("err");
+    });
 }
+
+function something() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+
+        var dataArr = data.split(",");
+
+        console.log(dataArr);
+
+    });
+};
